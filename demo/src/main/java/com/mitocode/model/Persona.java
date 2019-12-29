c09bca9236f22d6fa4c3566cb9d985ee8dc04be8
NestorPlasencia <nplasencia82@gmail.com>
2019-12-29 07:43:07 -0500

    añadir el modelo Persona

---
+++ demo/src/main/java/com/mitocode/model/Persona.java
@@ -0,0 +1,24 @@
+package com.mitocode.model;
+
+public class Persona {
+
+	private Integer idPersona;
+	private String nombre;		
+
+	public Integer getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(Integer idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
