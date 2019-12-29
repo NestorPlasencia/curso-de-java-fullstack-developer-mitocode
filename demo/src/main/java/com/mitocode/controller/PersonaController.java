ff3283c6a65d2326c4e4bf2020845bb9850cb732
NestorPlasencia <nplasencia82@gmail.com>
2019-12-29 16:16:33 -0500

    añadir el controlador de Persona

---
+++ demo/src/main/java/com/mitocode/controller/PersonaController.java
@@ -0,0 +1,23 @@
+package com.mitocode.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mitocode.model.Persona;
+import com.mitocode.service.IPersonaService;
+
+@RestController
+public class PersonaController {
+
+	@Autowired
+	private IPersonaService service;
+	
+	@GetMapping
+	public void saludar() {
+		Persona per = new Persona();
+		per.setIdPersona(1);
+		per.setNombre("Jaime");
+		service.saludar(per);
+	}
+}
--- demo/src/main/java/com/mitocode/service/PersonaServiceImpl.java
+++ demo/src/main/java/com/mitocode/service/PersonaServiceImpl.java
@@ -1,10 +1,12 @@
 package com.mitocode.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mitocode.model.Persona;
 import com.mitocode.repo.IPersonaRepo;
 
+@Service
 public class PersonaServiceImpl implements IPersonaService{
 
 	@Autowired
