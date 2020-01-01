4dcc2cf6b6fcbbbc62cfacc1697f2962764d8bcf
NestorPlasencia <nplasencia82@gmail.com>
2020-01-01 12:56:43 -0500

    añadir entidad DetalleVenta

---
+++ cineapp-backend/src/main/java/com/mitocode/model/DetalleVenta.java
@@ -0,0 +1,37 @@
+package com.mitocode.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "detalle_venta")
+public class DetalleVenta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idDetalle;
+
+	@Column(name = "asiento", length = 3)
+	private String asiento;
+
+	public Integer getIdDetalle() {
+		return idDetalle;
+	}
+
+	public void setIdDetalle(Integer idDetalle) {
+		this.idDetalle = idDetalle;
+	}
+
+	public String getAsiento() {
+		return asiento;
+	}
+
+	public void setAsiento(String asiento) {
+		this.asiento = asiento;
+	}
+
+}
