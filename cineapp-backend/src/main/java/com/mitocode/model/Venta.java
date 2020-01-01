917180d291f04ec2ac323d12fb3cea7185660d99
NestorPlasencia <nplasencia82@gmail.com>
2020-01-01 12:31:11 -0500

    añadir entidad Venta

---
+++ cineapp-backend/src/main/java/com/mitocode/model/Venta.java
@@ -0,0 +1,61 @@
+package com.mitocode.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "venta")
+public class Venta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idVenta;
+
+	@Column(name = "fecha")
+	private LocalDateTime fecha;
+
+	@Column(name = "cantidad", length = 2)
+	private Integer cantidad;
+
+	@Column(name = "total", columnDefinition = "decimal(5,2)")
+	private Double total;
+
+	public Integer getIdVenta() {
+		return idVenta;
+	}
+
+	public void setIdVenta(Integer idVenta) {
+		this.idVenta = idVenta;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+}
